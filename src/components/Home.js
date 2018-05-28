828e80650ab181f63cf567597ab97a1e50f991c2
edsant1 <edwins10329@gmail.com>
2018-05-27 19:35:16 -0600

    -added a few package.json dev-dependecies, changed files structure a bit

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import App from '../containers/App.js';
+
+class Home extends Component {
+	render() {
+		return(
+			<App>
+				<div>
+					<h3>working</h3>
+				</div>
+			</App>
+		);
+	}
+}
+
+export default Home;
